ffcbe47fc314a02ddc7dc49367783e7b5949ce98
Mathz9 <00001082679598sp@al.educacao.sp.gov.br>
2024-08-08 15:39:25 -0300

    Golfinho

---
+++ G.py
@@ -0,0 +1,15 @@
+Início
+    // Solicita ao usuário o primeiro número
+    Escreva "Digite o primeiro número: "
+    Leia numero1
+
+    // Solicita ao usuário o segundo número
+    Escreva "Digite o segundo número: "
+    Leia numero2
+
+    // Calcula a soma dos dois números
+    soma = numero1 + numero2
+
+    // Exibe o resultado
+    Escreva "A soma de ", numero1, " e ", numero2, " é ", soma
+Fim
